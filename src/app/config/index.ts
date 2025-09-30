a8d4c68f9cee7c5d4f6fc3d35fbc1788fd93437a
gsjoy24 <gour.joy24@gmail.com>
2025-09-30 20:42:14 +0600

    cc

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -11,5 +11,5 @@ export default {
   database_url: process.env.DATABASE_URL,
   bcrypt_salt_round: process.env.BCRYPT_SALT_ROUND,
   jwt_access_secret: process.env.JWT_ACCESS_TOKEN,
-  jwt_access_expiration: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_access_expiration: process.env.JWT_ACCESS_EXPIRES_IN
 };
